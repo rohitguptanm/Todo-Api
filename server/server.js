40c79bf6ca0777f52185924936ff212f9a056d64
Rohit <rohitguptanm@gmail.com>
2018-11-12 00:29:22 +0530

    environment set up

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config')
+
 const _ = require('lodash')
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,7 +12,7 @@ var { Users } = require('./models/Users');
 
 var app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // middleware
 app.use(bodyParser.json());
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,72 +5,72 @@ const { app } = require('../server')
 const { Todo } = require('../models/Todo')
 
 
-// // wipe out all the data of todo from db 
-// beforeEach((done) => {
-//     Todo.remove({}).then(() => done());
-// });
+// wipe out all the data of todo from db 
+beforeEach((done) => {
+    Todo.remove({}).then(() => done());
+});
 
-// describe('POST /todos', () => {
-//     it('should create a new todo', (done) => {
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
 
-//         var todo = new Todo({
-//             text: "My test",
-//             completed: true,
-//             completedAt: 12568
-//         });
+        var todo = new Todo({
+            text: "My test",
+            completed: true,
+            completedAt: 12568
+        });
 
-//         supertestRequest(app)
-//             .post('/todos')
-//             .send(todo)
-//             .expect(200)
-//             .expect((res) => {
-//                 expect(res.body.text).toBe(todo.text);
-//             })
-//             .end((err, res) => {
-//                 if (err) {
-//                     return done(err);
-//                 }
+        supertestRequest(app)
+            .post('/todos')
+            .send(todo)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(todo.text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
 
-//                 Todo.find().then((todos) => {
-//                     expect(todos.length).toBe(1);
-//                     expect(todos[0].text).toBe(todo.text);
-//                     done();
-//                 }).catch((e) => done(e));
-//             });
-//     });
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(todo.text);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
 
-//     it('Should not create todo with invalid body data', (done) => {
-//         supertestRequest(app)
-//             .post('/todos')
-//             .send({})
-//             .expect(400)
-//             .end((err, res) => {
-//                 if (err) {
-//                     return done(err);
-//                 }
+    it('Should not create todo with invalid body data', (done) => {
+        supertestRequest(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
 
-//                 Todo.find().then((todos) => {
-//                     expect(todos.length).toBe(0);
-//                     done();
-//                 }).catch((e) => done(e));
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
 
-//             });
-//     });
+            });
+    });
 
 
-// });
+});
 
 
 // get all data text case
-describe('GET /todos',()=>{
-    it('should get all todos',(done)=>{
-        supertestRequest(app)
-        .get('/todos')
-        .expect(200)
-        .expect((res)=>{
-            expect(res.body.todos.length).toBe(2);
-        })
-        .end(done)
-        .catch((e) => done(e));
-    })
-})
+// describe('GET /todos',()=>{
+//     it('should get all todos',(done)=>{
+//         supertestRequest(app)
+//         .get('/todos')
+//         .expect(200)
+//         .expect((res)=>{
+//             expect(res.body.todos.length).toBe(2);
+//         })
+//         .end(done)
+//         .catch((e) => done(e));
+//     })
+// })
