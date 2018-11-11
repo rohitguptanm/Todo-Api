284b87b7ce7e07a67303747d0353c6155ebe5d8d
Rohit <rohitguptanm@gmail.com>
2018-11-11 20:17:26 +0530

    setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
+    // auth: {
+    //   user: '<cosmosdb-username>',
+    //   password: '<cosmosdb-password>'
+    // }
+  })
+  .then(() => console.log('connection successful'))
+  .catch((err) => console.error(err));
 
 module.exports = { mongoose };
